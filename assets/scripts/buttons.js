5e3c8c8d1a77b41fc006fb2e7f52c30a76bcf7d2
Adam Tache <at201@duke.edu>
2018-10-03 22:07:50 -0600

    Fix homepage buttons hover image rendering

---
+++ assets/scripts/buttons.js
@@ -0,0 +1,42 @@
+function onWritingsHover()
+{
+    $(".writing-button").attr('src', 'assets/graphics/essays_research_hover.png');
+}
+
+function offWritingsHover()
+{
+    $(".writing-button").attr('src', 'assets/graphics/essays_research.png');
+    
+}
+
+function onMultimediaHover()
+{
+    $(".multimedia-button").attr('src', 'assets/graphics/videos_media_hover.png');
+}
+
+function offMultimediaHover()
+{
+    $(".multimedia-button").attr('src', 'assets/graphics/videos_media.png');
+    
+}
+
+function onProjectsCompaniesHover()
+{
+    $(".projects-companies-button").attr('src', 'assets/graphics/projects_companies_hover.png');
+}
+
+function offProjectsCompaniesHover()
+{
+    $(".projects-companies-button").attr('src', 'assets/graphics/projects_companies.png');
+    
+}
+
+function onBlogHover()
+{
+    $(".blog-button").attr('src', 'assets/graphics/theblog_hover.png');
+}
+
+function offBlogHover()
+{
+    $(".blog-button").attr('src', 'assets/graphics/theblog.png');
+}
